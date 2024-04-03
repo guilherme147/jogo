c314a415bff1ba18b3cc02d81691b34c99820240
Guilherme Veloso <guilherme_veloso1@estudante.sesisenai.org.br>
2024-04-02 21:27:07 -0300

    commit 1

---
+++ api.py
@@ -0,0 +1,126 @@
+import PySimpleGUI as sg
+sg.theme('DarkAmber')
+def comecarJogo():
+    layout = [
+        [sg.Image("image.png", key='-BACKGROUND-', enable_events=True)]
+    ]
+    window = sg.Window('jogo', layout)
+    while a == True:
+        event, values = window.read()
+        if(event == sg.WIN_CLOSED or event == 'Cancelar'):
+            break
+
+def adicionarJogador(jogadores, icon):
+    layout = [
+        [sg.Text('Deseja adicionar outro jogador?')],
+        [sg.Button('Sim'), sg.Button('Jogar')]
+    ]
+    window = sg.Window('registro', layout)
+
+    while a == True:
+        event, values = window.read()
+        if(event == sg.WIN_CLOSED or event == 'Cancelar'):
+            break
+        if(event == 'Sim'):
+            if(len(jogadores) > 4):
+                sg.popup_error("Ocorreu um erro! Voce atingiu o numero maximo de 5 jogadores! \n Iniciando jogo", title="Erro")
+                comecarJogo()
+                break
+            else:
+                criaJogadores(jogadores, icon)
+                break
+        if(event == 'Jogar'):
+            if(len(jogadores) < 2):
+                sg.popup_error("Ocorreu um erro! Para jogar deve ter no minimo 2 jogadoores", title="Erro")
+                criaJogadores(jogadores, icon)
+                break
+            else:
+                comecarJogo()
+                break
+    window.close()
+def criaJogadores(jogadores, icon):
+    # layout
+    layout = [
+        [sg.Text('Digite o seu nome'), sg.InputText()],
+        [sg.Combo(icon, 'Escolha um icon', s=(15,22), enable_events=True)],
+        [sg.Text('Digite sua senha'), sg.InputText(password_char='*')],
+        [sg.Button('OK'), sg.Button('Cancelar')]
+    ]
+    # janela
+    window = sg.Window('registro', layout)
+    # funcionamento
+    while a == True:
+        event, values = window.read()
+        valor1 = values[0]
+        valor2 = values[2]
+        print(valor1)
+        if(event == sg.WIN_CLOSED or event == 'Cancelar'):
+            break
+        if(event == 'OK'):
+            jogadores.append({"nome": valor1, "senha": valor2, "icon": values[1]})
+            print(jogadores)
+            window.close()
+            adicionarJogador(jogadores, icon)
+            break
+        if (event == 'Cancelar'):
+            break
+        
+    window.close()
+def sorteiaNu():
+    import random
+    num = random.randint(1, 6)
+    return num
+def criaTabuleiro():
+    tabuleiro = [0]
+    while(len(tabuleiro) < 51):
+        num = tabuleiro[-1] + 1
+        tabuleiro.append(num)
+        print("[",num,"]")
+criaTabuleiro()
+def cartas(posicao):
+    if(posicao == 3):
+        print("O jogador", nome , " ganhou uma carta e devera avançar 3 casas")
+        posicao += 3
+    elif(posicao == 9):
+        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para voltar 3 casas")
+        
+    elif(posicao == 14):
+        print("O jogador", nome , " ganhou uma carta e devera voltar 1 casa")
+        posicao -= 1
+    elif(posicao == 16):
+        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para voltar 2 casas")
+        posicao += 3
+    elif(posicao == 21):
+        print("O jogador", nome , " ganhou uma carta e devera voltar ao inicio")
+        posicao = 0
+    elif(posicao == 27):
+        print("O jogador", nome , " ganhou uma carta e devera avancar 3 casas")
+        posicao += 3
+    elif(posicao == 33):
+        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para voltar 3 casas")
+        posicao -= 3
+    elif(posicao == 37):
+        print("O jogador", nome , " ganhou uma carta e devera avancar 4 casas")
+        posicao += 4
+    elif(posicao == 39):
+        print("O jogador", nome , " ganhou uma carta e devera voltar 1 casa")
+        posicao -= 1
+    elif(posicao == 40):
+        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para avancar 2 casas")
+        posicao += 3
+    elif(posicao == 43):
+        print("O jogador", nome , " ganhou uma carta e devera avancar 1 casa")
+        posicao += 1
+    elif(posicao == 45):
+        print("O jogador", nome , " ganhou uma carta e devera voltar 3 casas")
+        posicao -= 3    
+    elif(posicao == 45):
+        print("O jogador", nome , " ganhou uma carta e devera voltar 2 casas")
+        posicao -= 2
+    elif(posicao == 49):
+        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para avancar 3 casas")
+        posicao += 3        
+jogadores = []   
+icon = [":D", ":)", ":|", ":(", ";("]
+a = True
+criaJogadores(jogadores, icon)
