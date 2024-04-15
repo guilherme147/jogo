76a8e9b95f1779ef53caf6daf3abaf420f1f5a92
Guilherme Veloso <guilherme_veloso1@estudante.sesisenai.org.br>
2024-04-15 19:37:22 -0300

    funcionalidades 2

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 import random
-sg.theme('DarkAmber')
+sg.theme('LightGrey1')
 def comecarJogo(jogadores):
     layout = [
         [sg.Image("image.png", key='-BACKGROUND-', enable_events=True)],
@@ -25,7 +25,17 @@ def comecarJogo(jogadores):
                     if((cartas(jogador, i['posicao'])) < i['posicao']):
                         i['posicao'] = cartas(jogador, i['posicao'])
                         sg.popup(f"O {i['nome']} voltou para posicao", i['posicao'])
-def adicionarJogador(jogadores, icon):
+                verificador = finalizaJogo(i['posicao'])
+                if(verificador == True):
+                    vencedor = jogador
+                    break
+            if(verificador == True):
+                    window.close()
+                    break
+                    
+                    
+        sg.popup('O jogador', vencedor,"ganhou o jogo.", title='Jogo')
+def adicionarJogador(jogadores):
     layout = [
         [sg.Text('Deseja adicionar outro jogador?')],
         [sg.Button('Sim'), sg.Button('Jogar')]
@@ -42,23 +52,21 @@ def adicionarJogador(jogadores, icon):
                 comecarJogo(jogadores)
                 break
             else:
-                criaJogadores(jogadores, icon)
+                criaJogadores(jogadores)
                 break
         if(event == 'Jogar'):
             if(len(jogadores) < 2):
                 sg.popup_error("Ocorreu um erro! Para jogar deve ter no minimo 2 jogadoores", title="Erro")
-                criaJogadores(jogadores, icon)
+                criaJogadores(jogadores)
                 break
             else:
                 comecarJogo(jogadores)
                 break
     window.close()
-def criaJogadores(jogadores, icon):
+def criaJogadores(jogadores):
     # layout
     layout = [
-        [sg.Text('Digite o seu nome'), sg.InputText()],
-        [sg.Combo(icon, 'Escolha um icon', s=(15,22), enable_events=True)],
-        [sg.Text('Digite sua senha'), sg.InputText(password_char='*')],
+        [sg.Text('Digite o seu nome'), sg.InputText(),sg.Image("alien.png", enable_events=True)],
         [sg.Button('OK'), sg.Button('Cancelar')]
     ]
     # janela
@@ -67,19 +75,17 @@ def criaJogadores(jogadores, icon):
     while a == True:
         event, values = window.read()
         valor1 = values[0]
-        valor2 = values[2]
         print(valor1)
         if(event == sg.WIN_CLOSED or event == 'Cancelar'):
             break
         if(event == 'OK'):
-            jogadores.append({"nome": valor1, "senha": valor2, "icon": values[1], "posicao": 0})
+            jogadores.append({"nome": valor1, "posicao": 0})
             print(jogadores)
             window.close()
-            adicionarJogador(jogadores, icon)
+            adicionarJogador(jogadores)
             break
         if (event == 'Cancelar'):
             break
-        
     window.close()
 def sorteiaNu():
     import random
@@ -129,15 +135,19 @@ def cartas(nome, posicao):
     elif(posicao == 45):
         print("O jogador", nome , " ganhou uma carta e devera voltar 3 casas")
         posicao -= 3    
-    elif(posicao == 45):
+    elif(posicao == 48):
         print("O jogador", nome , " ganhou uma carta e devera voltar 2 casas")
         posicao -= 2
-
     elif(posicao == 49):
         print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para avancar 3 casas")
         posicao += 3
-    return posicao        
+    return posicao  
+def finalizaJogo(posicao):
+    if(posicao > 51):
+        verificador = True
+    else:
+        verificador = False
+    return verificador
 jogadores = []   
-icon = [":D", ":)", ":|", ":(", ";("]
 a = True
-criaJogadores(jogadores, icon)
+criaJogadores(jogadores)
