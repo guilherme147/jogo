396a0ec755f06de13978d54d64a045c3c2d76eeb
Guilherme Veloso <guilherme_veloso1@estudante.sesisenai.org.br>
2024-04-15 21:10:15 -0300

    correcoes

--- api.py
+++ api.py
@@ -15,29 +15,31 @@ def comecarJogo(jogadores):
             for i in jogadores:
                 jogador = i['nome']
                 if(event == 'sorteio'):
-                    sg.popup('Vez do jogador', jogador, title='Jogo') 
+                    sg.popup(f"Vez do jogador {jogador}", title='Jogo') 
                     sorteio = random.randint(1, 6)
                     i['posicao'] = i['posicao'] + sorteio
-                    sg.popup(f"Saiu no dado {sorteio}", i['posicao'], title='Jogo')
+                    sg.popup(f"O dado sorteou o numero {sorteio}", f"O jogador está na posição {i['posicao']}", title='Jogo')
                     if((cartas(jogador, i['posicao'])) > i['posicao']):
                         i['posicao'] = cartas(jogador, i['posicao'])
-                        sg.popup(f"O {i['nome']} avancou para posicao", i['posicao'])
+                        sg.popup(f"O {i['nome']} tirou uma carta e avançou para posição {i['posicao']}")
                     if((cartas(jogador, i['posicao'])) < i['posicao']):
                         i['posicao'] = cartas(jogador, i['posicao'])
-                        sg.popup(f"O {i['nome']} voltou para posicao", i['posicao'])
+                        sg.popup(f"O {i['nome']} tirou uma carta e voltou para posição {i['posicao']}.")
                 verificador = finalizaJogo(i['posicao'])
+                if(event == sg.WIN_CLOSED or event == 'Cancelar'):
+                    window.close()
+                    break
                 if(verificador == True):
                     vencedor = jogador
                     break
+
             if(verificador == True):
                     window.close()
                     break
-                    
-                    
-        sg.popup('O jogador', vencedor,"ganhou o jogo.", title='Jogo')
+        sg.popup(f"O jogador {vencedor} ganhou o jogo!!!!", title='Jogo')
 def adicionarJogador(jogadores):
     layout = [
-        [sg.Text('Deseja adicionar outro jogador?')],
+        [sg.Text('Deseja adicionar outro jogador?'),sg.Image("alien.png", enable_events=True)],
         [sg.Button('Sim'), sg.Button('Jogar')]
     ] 
     window = sg.Window('registro', layout)
@@ -100,46 +102,32 @@ def criaTabuleiro():
 criaTabuleiro()
 def cartas(nome, posicao):
     if(posicao == 3):
-        print("O jogador", nome , " ganhou uma carta e devera avançar 3 casas")
         posicao += 3
     elif(posicao == 9):
-        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para voltar 3 casas")
         posicao -= 3
     elif(posicao == 14):
-        print("O jogador", nome , " ganhou uma carta e devera voltar 1 casa")
         posicao -= 1
     elif(posicao == 16):
-        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para voltar 2 casas")
         posicao += 3
     elif(posicao == 21):
-        print("O jogador", nome , " ganhou uma carta e devera voltar ao inicio")
         posicao = 0
     elif(posicao == 27):
-        print("O jogador", nome , " ganhou uma carta e devera avancar 3 casas")
         posicao += 3
     elif(posicao == 33):
-        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para voltar 3 casas")
         posicao -= 3
     elif(posicao == 37):
-        print("O jogador", nome , " ganhou uma carta e devera avancar 4 casas")
         posicao += 4
     elif(posicao == 39):
-        print("O jogador", nome , " ganhou uma carta e devera voltar 1 casa")
         posicao -= 1
     elif(posicao == 40):
-        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para avancar 2 casas")
         posicao += 3
     elif(posicao == 43):
-        print("O jogador", nome , " ganhou uma carta e devera avancar 1 casa")
         posicao += 1
     elif(posicao == 45):
-        print("O jogador", nome , " ganhou uma carta e devera voltar 3 casas")
         posicao -= 3    
     elif(posicao == 48):
-        print("O jogador", nome , " ganhou uma carta e devera voltar 2 casas")
         posicao -= 2
     elif(posicao == 49):
-        print("O jogador", nome , " ganhou uma carta e devera escolher um jogador para avancar 3 casas")
         posicao += 3
     return posicao  
 def finalizaJogo(posicao):
